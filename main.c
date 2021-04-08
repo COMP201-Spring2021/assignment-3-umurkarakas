12c87f884f44c29105aca15e3c94b33affa7ac27
Umur Berkay Karakas <ukarakas18@ku.edu.tr>
2021-04-08 12:33:16 +0300

    Implemented struct and initialWorld function

--- main.c
+++ main.c
@@ -9,24 +9,91 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#define FRATE 0.03
+#define RRATE 0.97
+#define INITIALCASES 0.05
 
 
+typedef struct Person {
+    char condition;
+    int pos;
+    int neighbors[8];
+    int sickCycles;
+} Person;
+
+Person** initialWorld(int n,int worldSize) {
+    Person** world = malloc(sizeof(Person*) * worldSize);
+    int i;
+    for (i = 0; i< worldSize; i++) {
+        world[i] = malloc(sizeof(Person));
+	world[i]->pos = i;
+        if((i % n) == 0) {
+            if(i / n == 0) {
+                world[i]->neighbors[0] = i+1;
+                world[i]->neighbors[1] = i+n;
+                world[i]->neighbors[2] = i+n+1;
+            } else if (i/n == n-1) {
+                world[i]->neighbors[0] = i-n;
+                world[i]->neighbors[1] = i-n+1;
+                world[i]->neighbors[2] = i+1;
+            } else {
+                world[i]->neighbors[0] = i-n;
+                world[i]->neighbors[1] = i-n+1;
+                world[i]->neighbors[2] = i+1;
+                world[i]->neighbors[3] = i+n;
+                world[i]->neighbors[4] = i+n+1;
+            }
+        } else if ((i % n) == n-1) {
+            if(i / n == 0) {
+                world[i]->neighbors[0] = i-1;
+                world[i]->neighbors[1] = i+n-1;
+                world[i]->neighbors[2] = i+n;
+            } else if (i/n == n-1) {
+                world[i]->neighbors[0] = i-n-1;
+                world[i]->neighbors[1] = i-n;
+                world[i]->neighbors[2] = i-1;
+            } else {
+                world[i]->neighbors[0] = i-n-1;
+                world[i]->neighbors[1] = i-n;
+                world[i]->neighbors[2] = i-1;
+                world[i]->neighbors[3] = i+n-1;
+                world[i]->neighbors[4] = i+n;
+            }
+        } else {
+            if(i / n == 0) {
+                world[i]->neighbors[0] = i-1;
+                world[i]->neighbors[1] = i+1;
+                world[i]->neighbors[2] = i+n-1;
+                world[i]->neighbors[3] = i+n;
+                world[i]->neighbors[4] = i+n+1;
+            } else if (i/n == n-1) {
+                world[i]->neighbors[0] = i-n-1;
+                world[i]->neighbors[1] = i-n;
+                world[i]->neighbors[2] = i-n+1;
+                world[i]->neighbors[3] = i-1;
+                world[i]->neighbors[4] = i+1;
+            } else {
+                world[i]->neighbors[0] = i-n-1;
+                world[i]->neighbors[1] = i-n;
+                world[i]->neighbors[2] = i-n+1;
+                world[i]->neighbors[3] = i-1;
+                world[i]->neighbors[4] = i+1;
+                world[i]->neighbors[5] = i+n-1;
+                world[i]->neighbors[6] = i+n;
+                world[i]->neighbors[7] = i+n+1;
+            }
+        }
+    }
+    return world;
+}
 
 int main( int argc, char *argv[] )  {
-
-   printf("Program name %s\n", argv[0]);
- 
-   if( argc == 5 ) {
-	printf("The first argument supplied is %d\n", atoi(argv[1]));
-	printf("The second argument supplied is %d\n", atoi(argv[2]));
-  	printf("The third argument supplied is %d\n", atoi(argv[3]));
-	printf("The forth argument supplied is %d\n", atoi(argv[4]));
-   }
-   else if( argc > 5 ) {
-      printf("Too many arguments supplied.\n");
-   }
-   else {
-      printf("Expected 4 arguments but only %d argument passed.\n", argc-1);
-   }
-      
+   int worldSize = (int) pow(atoi(argv[1]), 2);
+   double infectionChance = 1 - (atoi(argv[2])) / 100;
+   int simLength = atoi(argv[3]);
+   Person** world = initialWorld(atoi(argv[1]), worldSize);
+   int i;
+   
+   return 0;
 }
